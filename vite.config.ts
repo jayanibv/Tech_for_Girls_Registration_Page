b4b4d7a57d810183339f0fe9686aa5f7a4f4f4ee
jayanibv <jayanibv@gmail.com>
2025-07-08 12:13:00 +0530

    Added GitHub Actions workflow for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Tech_for_Girls_Registration_Page/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
